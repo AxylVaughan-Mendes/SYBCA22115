591d55f2296bc1816756a5a2d7ee623493ad063a
gilvinda dsouza <gilvindadsouza@gmail.com>
2024-01-10 10:31:30 +0530

    commmited

---
+++ git115/src/git115/gitprogram1.java
@@ -0,0 +1,16 @@
+package git115;
+
+public class gitprogram1 {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+		void newbranch() {
+			System.out.println("Branch commit");
+		
+
+	}
+
+}
