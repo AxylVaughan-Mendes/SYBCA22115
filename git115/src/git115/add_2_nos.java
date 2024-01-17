f3b2c847b64e598e6d85ebeab76833002a300943
gilvinda dsouza <gilvindadsouza@gmail.com>
2024-01-17 10:09:45 +0530

    commit

---
+++ git115/src/git115/add_2_nos.java
@@ -0,0 +1,19 @@
+package git115;
+
+import java.util.Scanner;
+
+public class add_2_nos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         Scanner scan= new Scanner(System.in);
+         int a,b,sum;
+         System.out.println("Enter the number");
+         a=scan.nextInt();
+         b=scan.nextInt();
+         sum=a+b;
+         System.out.println("the numbers are " +sum);
+
+         }
+
+}
